53ab5e02210a9e41227fa08d097daf0b31147d29
Swapnilrupakhetee <136236272+Swapnilrupakhetee@users.noreply.github.com>
2023-12-09 22:12:10 +0545

    hero-image-done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
+import Hero from './Components/Herosection/Hero';
 
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Hero/>
     
       
     </div>
--- src/Components/Herosection/Hero.jsx
+++ src/Components/Herosection/Hero.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Hero.css'
-
+import house from '../Assets/house-right.jpeg'
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero-wrapper'>
+      <div className='paddings innerWidth flexCenter hero-container'>
+        <div className='hero-left'>
+        </div>
+        <div className='flexCenter hero-right'>
+          <div className='image-container'>
+          <img src={house} alt='some-random-house'/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
