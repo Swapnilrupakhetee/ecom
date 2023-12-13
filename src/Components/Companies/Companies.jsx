4efc72e0a3fbf1001a6be16fd646fa5717b2bf07
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-13 21:05:05 +0545

    Added-companies

--- src/Components/Companies/Companies.jsx
+++ src/Components/Companies/Companies.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Companies.css'
-
+import { IoLogoTiktok } from "react-icons/io5";
+import { IoLogoMicrosoft } from "react-icons/io5";
+import { BiLogoGithub } from "react-icons/bi";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 const Companies = () => {
   return (
-    <div>Companies</div>
+    <div className='company-wrapper'>
+        <div className='paddings innerWidth flexCenter company-container'>
+        <IoLogoTiktok  className='logos'/>
+        <IoLogoMicrosoft className='logos'/>
+        <BiLogoGithub className='logos'/>
+        <BiLogoInstagramAlt className='logos'/>
+
+        </div>
+    </div>
   )
 }
 
