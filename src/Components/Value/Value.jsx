f9eb1e833f21f1c5eabc0c2d66a6cddef5e11532
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-14 20:42:38 +0545

    finised-onepart

--- src/Components/Value/Value.jsx
+++ src/Components/Value/Value.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../Value/Value.css'
+import React, { useState } from 'react'
+import '../Value/Value.css';
 import {
     Accordion,
     AccordionItem,
@@ -11,24 +11,67 @@ import {
 } from 'react-accessible-accordion';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import {MdOutlineArrowDropDown} from 'react-icons/md';
+import data from '../../utils/Accordion'
+
 
 
 const Value = () => {
+    const[className,setClassName]=useState(null)
   return (
     <section className="v-wrapper">
         <div className="paddings innerWidth flexCenter v-container">
             <div className="v-left">
                 <div className="image-container">
-                    <img src='./value.png' alt='ho'/>
+                    <img src='./s1.jpg' alt='ho'/>
                 </div>
             </div>
             <div className="flexColStart v-right">
-                <span>Our Value</span>
-                <span>Value We Give to You</span>
-                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, expedita cupiditate.
+                <span className='orangeText'>Our Value</span>
+                <span className='primaryText'>Value We Give to You</span>
+                <span className='secondaryText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, expedita cupiditate.
                     <br/>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, tempora voluptates.
                 </span>
+                <Accordion
+                className='accordion'
+                allowMultipleExpanded={false}
+                preExpanded={[0]}
+                >
+                {data.map((item,i)=>{
+                    
+                        return(
+                            <AccordionItem
+                            className={`accordionItem ${className}`} key={i} uuid={i}>
+                                <AccordionItemHeading>
+                                    <AccordionItemButton className='flexCenter accordionButton'>
+                                        <AccordionItemState>
+                                            {({expanded})=>expanded?setClassName("expnded"):setClassName("collasped")}
+                                        </AccordionItemState>
+                                        <div className="flexCenter icon">
+                                            {item.icon}
+                                        </div>
+                                        <span className="primaryText">
+                                            {item.heading}
+                                        </span>
+                                        <div className="flexCenter icon">
+                                            <MdOutlineArrowDropDown size={20}/>
+                                        </div>
+                                    </AccordionItemButton>
+                                    
+                                </AccordionItemHeading>
+                                <AccordionItemPanel>
+                                    <p className="secondary">{item.detail}</p>
+
+                                </AccordionItemPanel>
+
+                            </AccordionItem>
+
+                        );
+                    })
+
+                }
+
+                </Accordion>
             </div>
         </div>
     </section>
