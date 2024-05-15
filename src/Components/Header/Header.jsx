e06b68a54fdc86ba5a2ed4d900b06e8c6ee33a51
Swapnilrupakhetee <rupakheteeswapnil@email.com>
2024-05-15 17:53:41 +0545

    navbar works

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,43 +1,39 @@
-import React, { useState } from 'react'
-import './Header.css'
-import { BiMenuAltRight } from 'react-icons/bi'
-import OutsideClickHandler from 'react-outside-click-handler'
+import React, { useState } from 'react';
+import './Header.css';
+import { BiMenuAltRight } from 'react-icons/bi';
+import OutsideClickHandler from 'react-outside-click-handler';
+
 const Header = () => {
-  const[menuOpened,setMenuOpened] =useState(false)
-  const getMenuStyles=(menuOpened)=>{
-    if(document.documentElement.clientWidth <=800)
-    {
-      return{right:menuOpened===false && "-100%"}
-    }
-  }
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpened(prev => !prev);
+  };
+
   return (
     <div className='header-wrapper'>
-        <div className='flexCenter paddings innerWidth header-container'>
-            <h1>LOGO</h1>
+      <div className='flexCenter paddings innerWidth header-container'>
+        <h1>LOGO</h1>
 
-
-            <OutsideClickHandler
-            onOutsideClick={()=>{
-              setMenuOpened(false);
-            }}>
-            <div className='flexCenter header-menu'
-            style={getMenuStyles(menuOpened)}>
-                <a href=''>Residencies</a>
-                <a href=''>Value</a>
-                
-                <a href=''>Get Started</a>
-                <button className='button'><a href=''>Contact US</a></button>
-                
-            </div>
-            </OutsideClickHandler>
-            <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
-              <BiMenuAltRight size={30}/>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+          <div
+            className={`flexCenter header-menu ${menuOpened ? 'visible' : ''}`}
+            style={{ right: menuOpened ? '0' : '-100%' }}
+          >
+            <a href=''>Residencies</a>
+            <a href=''>Value</a>
+            <a href=''>Get Started</a>
+            <button className='button'>
+              <a href=''>Contact US</a>
+            </button>
           </div>
-            
+        </OutsideClickHandler>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <BiMenuAltRight size={30} />
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
